cbdb304ff4cbc1819529a303c5222561b64e2fe3
Laoujin <woutervs@hotmail.com>
2014-05-24 19:18:40 +0200

    wait for document.ready before executing sangu_ready

--- greasemonkey/inject.js
+++ greasemonkey/inject.js
@@ -1,7 +1,7 @@
 (function (func) {
     var script = document.createElement('script');
     script.setAttribute("type", "application/javascript");
-    script.textContent = '(' + func + ')();';
+    script.textContent = '$(document).ready(' + func + ')();';
     document.body.appendChild(script); // run the script
     document.body.removeChild(script); // clean up
 }(sangu_ready));
