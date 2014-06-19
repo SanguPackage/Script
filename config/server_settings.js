0a4e445b192672a6275b7df11d77de1447c773e7
Laoujin <woutervs@hotmail.com>
2014-06-19 10:28:01 +0200

    set version to 8.24 - TW and Sangu versions now match

--- config/server_settings.js
+++ config/server_settings.js
@@ -2,7 +2,7 @@ server_settings = {
     /**
      * When game_data.majorVersion is different from this version then activate sangu 'compatibility' mode (gray icon)
      */
-    tw_version: "8.23",
+    tw_version: "8.24",
     /**
      * Calculate how many more days we can attack in vacation mode
      */
