d33344ca80617924512e2fc2e19335c8a1b08ce4
Laoujin <woutervs@hotmail.com>
2014-06-19 11:31:15 +0200

    document.ready wait only for firefox

--- greasemonkey/inject.js
+++ greasemonkey/inject.js
@@ -1,7 +1,17 @@
 (function (func) {
     var script = document.createElement('script');
     script.setAttribute("type", "application/javascript");
-    script.textContent = '$(document).ready(' + func + ');';
+    if (window.mozInnerScreenX !== undefined) {
+        // Firefox has troubles with renaming commands, villages, ... (it works some of the time)
+        // But waiting for document.ready slows down the script so only wait for this on FF
+        // An optimization could be to put document.readys only around those blocks that are
+        // problematic.
+        script.textContent = '$(document).ready(' + func + ');';
+
+    } else {
+        script.textContent = '(' + func + ')();';
+    }
+
     document.body.appendChild(script); // run the script
     document.body.removeChild(script); // clean up
 }(sangu_ready));
