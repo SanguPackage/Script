65a9b7cff303c8e62ac55fec0530683e4cee470e
Jimmy <jimmyj@live.nl>
2014-08-12 23:59:27 +0200

    //Hardcode Fixed that the function didnt work. Added a plus and minus image to make it easier for the users

--- page/info_village.js
+++ page/info_village.js
@@ -1,18 +1,18 @@
 //Written by hardcode
-function hide_incommings(){
+(function() {
     var show = true;
     var head = $(".vis:contains('Aankomend')");
-    var text = $("th:contains('Aankomst:')").attr("id","text");
+    var text = $("th:contains('Aankomst:')").attr({id:"text"}).html('Aankomend: <img src="http://nlp1.tribalwars.nl/graphic/minus.png" style=" float: right;">');
 
     head.on("click", function () {
         if(show === true) {
             $(".no_ignored_command").hide();
-            text.html("Hidden");
+            text.html('Hidden <img src="http://nlp1.tribalwars.nl/graphic/plus.png" style=" float: right;">');
             show = false;
         }else{
             $(".no_ignored_command").show();
-            text.html("Aankomend:");
+            text.html('Aankomend: <img src="http://nlp1.tribalwars.nl/graphic/minus.png" style=" float: right;">');
             show = true;
         }
     });
-}
+}());
