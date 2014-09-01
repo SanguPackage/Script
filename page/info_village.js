db33a0029d23e87d9fd22e51bca2bc6bb1b62591
Laoujin <woutervs@hotmail.com>
2014-09-01 22:01:52 +0200

    Enhancement: incoming attacks only hide/show on header (to allow copy-paste)

--- page/info_village.js
+++ page/info_village.js
@@ -1,13 +1,13 @@
 //Written by hardcode
 (function() {
     var show = true;
-    var head = $(".vis:contains('Aankomend')");
-    var text = $("th:contains('Aankomst:')").attr({id:"text"}).html('Aankomend: <img src="http://nlp1.tribalwars.nl/graphic/minus.png" style=" float: right;">');
+    var head = $(".vis:contains('Aankomend')", content_value).find("tr:first");
+    var text = $("th:contains('Aankomst:')", content_value).html('Aankomend: <img src="http://nlp1.tribalwars.nl/graphic/minus.png" style=" float: right;">');
 
     head.on("click", function () {
         if(show === true) {
             $(".no_ignored_command").hide();
-            text.html('Hidden <img src="http://nlp1.tribalwars.nl/graphic/plus.png" style=" float: right;">');
+            text.html('Verborgen: <img src="http://nlp1.tribalwars.nl/graphic/plus.png" style=" float: right;">');
             show = false;
         }else{
             $(".no_ignored_command").show();
