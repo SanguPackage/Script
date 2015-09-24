90a9d5d98fd49c9269413a320da4ec2fea5caa77
Tjeerdo <tjeppo@hotmail.com>
2015-09-24 14:27:12 +0200

    fix for show distance/speed place

--- page/place/lastandtargetvillage.js
+++ page/place/lastandtargetvillage.js
@@ -9,14 +9,14 @@
             dist = getDistance(coord.x, vilHome.x, coord.y, vilHome.y, speedCookie);
             var htmlStr = printCoord(coord, "&raquo; " + trans.sp.all.last + ": " + coord.x + "|" + coord.y);
             htmlStr += " &nbsp; <span id=lastVilTime>" + dist.html + "</span>";
-            $("#units_form").append(htmlStr);
+            $("#command-data-form").append(htmlStr);
         }
 
         // Add target village
         var targetVillage = getVillageFromCoords(spTargetVillageCookie());
         if (targetVillage.isValid) {
             dist = getDistance(targetVillage.x, vilHome.x, targetVillage.y, vilHome.y, speedCookie);
-            $("#units_form").append("<br>" + printCoord(targetVillage, "&raquo; " + trans.sp.all.target + ": " + targetVillage.x + "|" + targetVillage.y) + " &nbsp;<span id=targetVilTime>" + dist.html + "</span>");
+            $("#command-data-form").append("<br>" + printCoord(targetVillage, "&raquo; " + trans.sp.all.target + ": " + targetVillage.x + "|" + targetVillage.y) + " &nbsp;<span id=targetVilTime>" + dist.html + "</span>");
         }
     } catch (e) { handleException(e, "place-lastandtargetvillage"); }
 }());
